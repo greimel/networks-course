aa4d5dcaaced99b4538aab43080c4eee72520993
greimel <fabgrei@gmail.com>
2021-01-25 16:21:11 +0000

    Deploying to gh-pages from  @ d6ee05f509b179cb6a17dc775c2627c138561ba6 🚀

--- notebooks/first-networks.jl
+++ notebooks/first-networks.jl
@@ -37,7 +37,7 @@ In this section we show you how to create networks in Julia and how to visualize
 
 # ╔═╡ df4d9fab-13da-4df7-b51e-0689112f65fe
 md"""
-## Graphs with names
+## Networks with names
 
 Let us plot our first networks. Below you see *star network* (can you imagine why it is called that way?). You can specify it by
 """
@@ -68,7 +68,7 @@ Try it and visualize a few graphs!
 # ╔═╡ b01cef89-6258-4050-9d35-7628eaf54010
 begin
 	my_network = SimpleGraph(7)
-	add_edge!(my_network, 1, 2)
+	add_edge!(my_network, 3, 4)
 	add_edge!(my_network, 2, 3)
 end
 
@@ -122,6 +122,8 @@ md"""
 # Analyzing networks with LightGraphs.jl
 
 **NOTE** The concepts of this section are introduced in the first lectures of the course. Have a look if you're curious. But it is rather meant as a references for your assignments.
+
+If you cannot find what you need in this notebook, check out the excellent [documentation of LightGraphs.jl](https://juliagraphs.org/LightGraphs.jl/stable).
 """
 
 # ╔═╡ 7e163209-7c52-4116-bcc2-572060b90fde
@@ -168,7 +170,50 @@ We can have a look at the full degree distribution by plotting a histogram.
 histogram(degrees)
 
 # ╔═╡ a5a085cf-b4dd-48c0-a3c2-967abc1445c2
+mean(degrees)
 
+# ╔═╡ b0ab1ac5-8433-4818-8579-f2c36d0dee30
+std(degrees)
+
+# ╔═╡ 5d7adf23-4fef-4597-a3ac-18adbef08d8e
+md"""
+## Components, path length and diameter
+
+"""
+
+# ╔═╡ 7ba0f472-f8a3-497d-8093-6f9275365841
+global_clustering_coefficient(big_network)
+
+# ╔═╡ 9f083058-6a12-41cc-bb65-ad81e5d79aea
+diameter(big_network)
+
+# ╔═╡ 2c703f99-5d25-44db-8651-92dd6427a605
+diameter(simple_network)
+
+# ╔═╡ 7381cca1-5f12-48d3-8a33-4642e8f80072
+components = connected_components(my_network)
+
+# ╔═╡ 9c3d3a6a-4ad5-4c45-bb07-8e75b4380290
+function giant_component(graph)
+	components = connected_components(graph)
+	
+	# compute the size (# of nodes) of each component
+	size_of_components = length.(components)
+	# find the component with maximal number of nodes
+	(n_nodes, ind) = findmax(size_of_components)
+	
+	# return the giant_component
+	giant_component = components[ind]
+end
+
+# ╔═╡ 2ec96593-85fa-4f45-aceb-f3869717884e
+giant_component(my_network)
+
+# ╔═╡ 7f457cac-c153-44a8-a13c-af03ffd6eef1
+subgraph, node_list = induced_subgraph(my_network, giant_component(my_network))
+
+# ╔═╡ ba4ddf01-d02e-4d9f-beb7-15467a03b08a
+gplot(subgraph)
 
 # ╔═╡ ef85efd2-da5c-4197-831e-110aebe5a1d7
 plot(x -> log(1 - ecdf(degrees)(x)), 1, 250)
@@ -179,8 +224,40 @@ diameter(big_network)
 # ╔═╡ 2e02bf8a-b9f2-4aaf-8e58-e5d17e3d193c
 is_connected(big_network)
 
-# ╔═╡ 09095fa6-5aa3-4d4b-88a5-a3e04d701837
-#gdistances
+# ╔═╡ 0f3c851f-78ea-4d0f-bfcf-7a6f1df9c152
+# Todo: check if this needs to be transposed
+function distance_matrix(graph)
+	n = nv(graph) # number of vertices
+	
+	distance_matrix = zeros(Int, n, n)
+	
+	for (i, node) in enumerate(vertices(graph))
+		distance_matrix[i, :] .= gdistances(graph, node)
+	end
+	
+	distance_matrix
+end
+
+# ╔═╡ 7c308142-d5b5-47c0-be74-083709e43ac5
+distance_matrix(simple_network)
+
+# ╔═╡ f609d59f-25ce-4075-a824-c96bc4e9bbe3
+md"
+## Centralities
+"
+
+# ╔═╡ 12cfd4cd-3448-405a-b8bb-ad1d73c23150
+katz_centrality(big_network)
+# katz_centrality(big_network, 0.3)
+
+# ╔═╡ ec57d7c7-0a96-40a4-942f-73723460a5fe
+betweenness_centrality(simple_network)
+
+# ╔═╡ 0d659ab1-88ce-48ce-8ee0-83185fd865aa
+eigenvector_centrality(simple_network)
+
+# ╔═╡ 7883f729-f34d-4a1c-a684-6d78700d2a45
+closeness_centrality(simple_network)
 
 # ╔═╡ 1250300d-8bd5-41c3-a36f-b59064e8fbfd
 md"""
@@ -217,10 +294,26 @@ TableOfContents()
 # ╟─313359ed-a84c-4eb8-86da-3da41cf475d4
 # ╠═a1fe05e9-b3e3-4055-831c-ca6289086fbe
 # ╠═a5a085cf-b4dd-48c0-a3c2-967abc1445c2
+# ╠═b0ab1ac5-8433-4818-8579-f2c36d0dee30
+# ╟─5d7adf23-4fef-4597-a3ac-18adbef08d8e
+# ╠═7ba0f472-f8a3-497d-8093-6f9275365841
+# ╠═9f083058-6a12-41cc-bb65-ad81e5d79aea
+# ╠═2c703f99-5d25-44db-8651-92dd6427a605
+# ╠═7381cca1-5f12-48d3-8a33-4642e8f80072
+# ╠═9c3d3a6a-4ad5-4c45-bb07-8e75b4380290
+# ╠═2ec96593-85fa-4f45-aceb-f3869717884e
+# ╠═7f457cac-c153-44a8-a13c-af03ffd6eef1
+# ╠═ba4ddf01-d02e-4d9f-beb7-15467a03b08a
 # ╠═ef85efd2-da5c-4197-831e-110aebe5a1d7
 # ╠═62063f20-4041-454d-964b-e2e89a8634f0
 # ╠═2e02bf8a-b9f2-4aaf-8e58-e5d17e3d193c
-# ╠═09095fa6-5aa3-4d4b-88a5-a3e04d701837
+# ╠═0f3c851f-78ea-4d0f-bfcf-7a6f1df9c152
+# ╠═7c308142-d5b5-47c0-be74-083709e43ac5
+# ╟─f609d59f-25ce-4075-a824-c96bc4e9bbe3
+# ╠═12cfd4cd-3448-405a-b8bb-ad1d73c23150
+# ╠═ec57d7c7-0a96-40a4-942f-73723460a5fe
+# ╠═0d659ab1-88ce-48ce-8ee0-83185fd865aa
+# ╠═7883f729-f34d-4a1c-a684-6d78700d2a45
 # ╟─1250300d-8bd5-41c3-a36f-b59064e8fbfd
 # ╠═c5cf8e17-9dcc-4f37-ace2-dbc3d92a83d4
 # ╠═2ecf4ffd-d41d-494c-9fec-d681a176a8ba
