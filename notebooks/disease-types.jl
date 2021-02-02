5a9b70ad1a40074231a2e8502f1dbcdd62b400de
Fabian Greimel <fabgrei@gmail.com>
2021-02-02 17:42:55 +0100

    put figure in the introduction

--- notebooks/disease-types.jl
+++ notebooks/disease-types.jl
@@ -43,6 +43,11 @@ begin
 	_a_
 end
 
+# ╔═╡ 7666d01c-6575-11eb-0197-8d34dd66d720
+md"""
+**NOTE!** This notebook does not run in Safari!
+"""
+
 # ╔═╡ f4266196-64aa-11eb-3fc1-2bf0e099d19c
 md"""
 # Diffusion on Networks: Modeling Transmission of Disease
@@ -136,6 +141,14 @@ function iterate!(states_new, states, adjacency_matrix, par)
 	states_new
 end
 
+# ╔═╡ 5d11a2df-3187-4509-ba7b-8388564573a6
+function iterate(states, adjacency_matrix, par)
+	states_new = Vector{States}(undef, N)
+	iterate!(states_new, states, adjacency_matrix, par)
+	
+	states_new
+end
+
 # ╔═╡ 07f4816c-b893-4771-be3f-cc10695720cf
 md"""
 ## Functions for the simulation
@@ -155,7 +168,7 @@ function initial_state(N, n_infected)
 end
 
 # ╔═╡ 208445c4-5359-4442-9b9b-bde5e55a8c23
-function simulate(graph, par, T, init = initial_state(nv(graph), 10))
+function simulate(graph, par, T, init = initial_state(nv(graph), max(nv(graph) ÷ 100, 1)))
 	mat = adjacency_matrix(graph)
 	N = nv(graph)
 	
@@ -170,24 +183,22 @@ end
 
 # ╔═╡ 50d9fb56-64af-11eb-06b8-eb56903084e2
 md"""
-## Simulate
+## Simulate on a Simple Network
 
-### Choosing the network
+### Setting the Parameters
+
+* ``\rho_s``: $(@bind ρ_simple PlutoUI.Slider(0.0:0.25:1.0, default = 0.0, show_value =true)) (recovery probability)
+* ``\delta_s``: $(@bind δ_simple PlutoUI.Slider(0.0:0.25:1.0, default = 0.0, show_value =true)) (death rate)
+* ``p_s``: $(@bind p_simple PlutoUI.Slider(0.0:0.25:1.0, default = 0.5, show_value =true)) (infection probability)
 """
 
-# ╔═╡ 213510df-9772-4be9-b58d-db173c21519a
-N = 1000
+# ╔═╡ 8d4cb5dc-6573-11eb-29c8-81baa6e3fffc
+simple_graph = CycleGraph(10)
 
-# ╔═╡ 5d11a2df-3187-4509-ba7b-8388564573a6
-function iterate(states, adjacency_matrix, par)
-	states_new = Vector{States}(undef, N)
-	iterate!(states_new, states, adjacency_matrix, par)
-	
-	states_new
-end
-
-# ╔═╡ 952f2540-64b4-11eb-0563-673747769a61
-
+# ╔═╡ ce75fe16-6570-11eb-3f3a-577eac7f9ee8
+md"""
+## Simulate on a Big Network
+"""
 
 # ╔═╡ 37972f08-db05-4e84-9528-fe16cd86efbf
 md"""
@@ -197,9 +208,6 @@ md"""
 * ``\delta``: $(@bind δ0 PlutoUI.Slider(0.0:0.02:0.2, default = 0.04, show_value =true)) (death rate)
 * ``p``: $(@bind p0 PlutoUI.Slider(0.1:0.1:0.9, default = 0.3, show_value =true)) (infection probability)
 """
-
-# ╔═╡ 2d3466df-48b3-432f-843d-8f83d7fb575e
-par = (ρ = ρ0, δ = δ0, p = p0)
 
 # ╔═╡ 47ac6d3c-6556-11eb-209d-f7a8219512ee
 md"""
@@ -217,6 +225,8 @@ function plot_fractions!(figpos, legpos, t, df, color_dict)
 	end
 	
 	vlines!(ax, @lift([$t]), color = :gray50, linestyle=(:dash, :loose))
+	
+	AbstractPlotting.ylims!(ax, -0.05, 1.05)
 	
 	leg = Legend(legpos, ax)
 	leg.tellwidth = false
@@ -247,8 +257,12 @@ function plot_diffusion!(figpos, edges_as_pts, node_positions, sim, t, color_dic
 	hidedecorations!(ax)
     #hidespines!(ax)
 
+	N, T = size(sim)
+	msize = N < 20 ? 10 : N < 100 ? 5 : 3
+	
+	
 	AbstractPlotting.lines!(ax, edges_as_pts, linewidth = 0.1, color = (:black, 0.1))
-    AbstractPlotting.scatter!(ax, node_positions, markersize=3, color = state_as_color_t);
+    AbstractPlotting.scatter!(ax, node_positions, markersize=msize, color = state_as_color_t);
 	
 	(; ax)
 end
@@ -279,9 +293,14 @@ end
 # ╔═╡ bf18bef2-649d-11eb-3e3c-45b41a3fa6e5
 function fractions_over_time(sim)
 	tidy_sim = tidy_simulation_output(sim)
+	N, T = size(sim)
+	
 	
 	combine(groupby(tidy_sim, [:t, :state]), :node_id => (x -> length(x)/N) => :fraction)
 end
+
+# ╔═╡ 70605248-64b1-11eb-0c7c-b50c8394cdb6
+tidy_simulation_output(sim)
 
 # ╔═╡ 4dee5da9-aa4b-4551-974a-f7268d016617
 md"""
@@ -295,91 +314,6 @@ md"""
 
 # ╔═╡ 7b43d3d6-03a0-4e0b-96e2-9de420d3187f
 p_range = 0.1:0.1:0.9
-
-# ╔═╡ b44bf44f-7041-409a-aea2-7652f18853b0
-md"""
-## Vaccination
-
-There are $N people and you can distribute $(N ÷ 10) doses of the vaccine. Whom would you vaccinate?
-"""
-
-# ╔═╡ 04227a80-5d28-43db-929e-1cdc5b31796d
-md"""
-## Travel ban
-
-remove links
-"""
-
-# ╔═╡ fb4ff86c-64ad-11eb-2962-3372a2f2d9a5
-md"""
-# Assignment 2: Whom to Vaccinate When Death Rates are Age-Specfic
-"""
-
-# ╔═╡ 1b8c26b6-64aa-11eb-2d9a-47db5469a654
-md"""
-# Appendix
-"""
-
-# ╔═╡ e82d5b7f-5f37-4696-9917-58b117b9c1d6
-md"
-## Spatial graph
-"
-
-# ╔═╡ 95b67e4d-5d41-4b86-bb9e-5de97f5d8957
-# adapted from David Gleich, Purdue University
-# https://www.cs.purdue.edu/homes/dgleich/cs515-2020/julia/viral-spreading.html
-function spatial_graph(node_positions; degreedist = LogNormal(log(2),1))
-  	n = length(node_positions)
-	
-	coords_matrix = hcat(Vector.(node_positions)...)
-  	T = BallTree(coords_matrix)
-	
-	g = SimpleGraph(n)
-	
-	for i = 1:n
-		# draw the number of links `deg`
-    	deg = min(ceil(Int, rand(degreedist)), n - 1)
-    	# use the `deg` closest nodes as neighbours
-		idxs, dists = knn(T, coords_matrix[:,i], deg + 1)
-    	for j in idxs
-      		if i != j
-				add_edge!(g, i, j)
-      		end
-    	end
-  	end
-	
-	g
-end
-
-# ╔═╡ c1971734-2299-4038-8bb6-f62d020f92cb
-function spatial_graph(N::Int)
-	id = 1:N
-	x = rand(N)
-	y = rand(N)
-	node_positions = Point2f0.(x, y)
-	
-	spatial_graph(node_positions), node_positions
-end
-
-# ╔═╡ bea21e32-7b13-4772-9e34-9b4b1f3333fb
-begin
-	graph, node_positions = spatial_graph(N)
-end
-
-# ╔═╡ 0b35f73f-6976-4d85-b61f-b4188440043e
-begin
-	T = 100
-	sim = simulate(graph, par, T)	
-	nothing
-end
-
-# ╔═╡ d910af76-64b2-11eb-14b6-a7f62a6ad82e
-begin
-	@bind t0 PlutoUI.Slider(1:T, show_value = true, default = 1)
-end
-
-# ╔═╡ 70605248-64b1-11eb-0c7c-b50c8394cdb6
-tidy_simulation_output(sim)
 
 # ╔═╡ c5f48079-f52e-4134-8e6e-6cd4c9ee915d
 let
@@ -398,6 +332,13 @@ let
 	
 	plt
 end
+
+# ╔═╡ b44bf44f-7041-409a-aea2-7652f18853b0
+md"""
+## Vaccination
+
+There are $N people and you can distribute $(N ÷ 10) doses of the vaccine. Whom would you vaccinate?
+"""
 
 # ╔═╡ ee230ae8-8885-4b40-9ad1-87ae295f11c1
 n_contacts = degree_centrality(graph, normalize = false)
@@ -457,6 +398,64 @@ let
 	plt
 end
 
+# ╔═╡ 04227a80-5d28-43db-929e-1cdc5b31796d
+md"""
+## Travel ban
+
+remove links
+"""
+
+# ╔═╡ fb4ff86c-64ad-11eb-2962-3372a2f2d9a5
+md"""
+# Assignment 2: Whom to Vaccinate When Death Rates are Age-Specfic
+"""
+
+# ╔═╡ 1b8c26b6-64aa-11eb-2d9a-47db5469a654
+md"""
+# Appendix
+"""
+
+# ╔═╡ e82d5b7f-5f37-4696-9917-58b117b9c1d6
+md"
+## Spatial graph
+"
+
+# ╔═╡ 95b67e4d-5d41-4b86-bb9e-5de97f5d8957
+# adapted from David Gleich, Purdue University
+# https://www.cs.purdue.edu/homes/dgleich/cs515-2020/julia/viral-spreading.html
+function spatial_graph(node_positions; degreedist = LogNormal(log(2),1))
+  	n = length(node_positions)
+	
+	coords_matrix = hcat(Vector.(node_positions)...)
+  	T = BallTree(coords_matrix)
+	
+	g = SimpleGraph(n)
+	
+	for i = 1:n
+		# draw the number of links `deg`
+    	deg = min(ceil(Int, rand(degreedist)), n - 1)
+    	# use the `deg` closest nodes as neighbours
+		idxs, dists = knn(T, coords_matrix[:,i], deg + 1)
+    	for j in idxs
+      		if i != j
+				add_edge!(g, i, j)
+      		end
+    	end
+  	end
+	
+	g
+end
+
+# ╔═╡ c1971734-2299-4038-8bb6-f62d020f92cb
+function spatial_graph(N::Int)
+	id = 1:N
+	x = rand(N)
+	y = rand(N)
+	node_positions = Point2f0.(x, y)
+	
+	spatial_graph(node_positions), node_positions
+end
+
 # ╔═╡ 5fe4d47c-64b4-11eb-2a44-473ef5b19c6d
 md"""
 ## Utils
@@ -477,7 +476,7 @@ function edges_as_points(graph, node_positions)
 end
 
 # ╔═╡ 51a16fcc-6556-11eb-16cc-71a978e02ef0
-function sir_plot(sim, graph, node_positions)
+function sir_plot(sim, graph, node_positions = NetworkLayout.Spring.layout(adjacency_matrix(graph), Point2f0))
 	t = Node(1)
 	
 	df = fractions_over_time(sim)
@@ -488,25 +487,71 @@ function sir_plot(sim, graph, node_positions)
 	colors = cgrad(:viridis, length(states), categorical=true)
 	color_dict = Dict(s => colors[i] for (i,s) in enumerate(states))
 	
-	sir_fig = Figure()
-	main_fig = sir_fig[2,1]
-	leg_pos = sir_fig[1,1]
+	fig = Figure()
+	main_fig = fig[2,1]
+	leg_pos = fig[1,1]
 
 	plot_fractions!(main_fig[1,2], leg_pos, t, df, color_dict)
 	plot_diffusion!(main_fig[1,1], edges_as_pts, node_positions, sim, t, color_dict)
 
-	(; t, sir_fig)
+	(; t, fig, T_range = axes(sim, 2))
 end 
 
-# ╔═╡ f9ce314c-655d-11eb-135b-2df45d413ab1
-begin
-	t, sir_fig = sir_plot(sim, graph, node_positions)
-	nothing
-	sir_fig
-end
+# ╔═╡ d6694c32-656c-11eb-0796-5f485cccccf0
+out_simple = let
+	T = 10
+	
+	par = (ρ = ρ_simple, δ = δ_simple, p = p_simple)
+	
+	sim = simulate(simple_graph, par, T)
+	
+	sir_plot(sim, simple_graph)
+	
+end;	
+
+# ╔═╡ 9302b00c-656f-11eb-25b3-495ae1c843cc
+md"""
+``t``: $(@bind t0_simple NumberField(out_simple.T_range, default=1))
+"""
+
+# ╔═╡ 657c3a98-6573-11eb-1ccb-b1d974414647
+out_simple.fig
+
+# ╔═╡ d2813d40-656d-11eb-2cfc-e389ed2a0d84
+out_simple.t[] = t0_simple
+
+# ╔═╡ 0b35f73f-6976-4d85-b61f-b4188440043e
+out_big = let
+	T = 100
+	
+	par = (ρ = ρ0, δ = δ0, p = p0)
+	
+	graph, node_positions = spatial_graph(1000)
+	sim = simulate(graph, par, T)	
+	
+	out_big = sir_plot(sim, graph, node_positions)
+end;
+
+# ╔═╡ 43a25dc8-6574-11eb-3607-311aa8d5451e
+md"""
+``t``: $(@bind t0_intro PlutoUI.Slider(out_big.T_range, show_value = true, default = 20))
+"""
+
+# ╔═╡ 3e9af1f4-6575-11eb-21b2-453dc18d1b7b
+out_big.fig
+
+# ╔═╡ 1bd2c660-6572-11eb-268c-732fd2210a58
+out_big.fig
+
+# ╔═╡ f4cd5fb2-6574-11eb-37c4-73d4b21c1883
+md"""
+Check to activate slider: $(@bind past_intro CheckBox(default = false))
+
+``t``: $(@bind t0_big PlutoUI.Slider(out_big.T_range, show_value = true, default = 1))
+"""
 
 # ╔═╡ 373cb47e-655e-11eb-2751-0150985d98c1
-t[] = t0
+out_big.t[] = past_intro ? t0_big : t0_intro
 
 # ╔═╡ a81f5244-64aa-11eb-1854-6dbb64c8eb6a
 md"""
@@ -525,7 +570,10 @@ Base.show(io::IO, ::MIME"text/html", x::CategoricalArrays.CategoricalValue) = pr
 TableOfContents()
 
 # ╔═╡ Cell order:
+# ╟─7666d01c-6575-11eb-0197-8d34dd66d720
 # ╟─f4266196-64aa-11eb-3fc1-2bf0e099d19c
+# ╟─43a25dc8-6574-11eb-3607-311aa8d5451e
+# ╟─3e9af1f4-6575-11eb-21b2-453dc18d1b7b
 # ╟─b36832aa-64ab-11eb-308a-8f031686c8d6
 # ╟─c8f92204-64ac-11eb-0734-2df58e3373e8
 # ╟─2f9f008a-64aa-11eb-0d9a-0fdfc41d4657
@@ -542,16 +590,18 @@ TableOfContents()
 # ╠═fecf62c5-2c1d-4709-8c17-d4b6e0565617
 # ╠═208445c4-5359-4442-9b9b-bde5e55a8c23
 # ╟─50d9fb56-64af-11eb-06b8-eb56903084e2
-# ╠═213510df-9772-4be9-b58d-db173c21519a
-# ╠═bea21e32-7b13-4772-9e34-9b4b1f3333fb
-# ╠═952f2540-64b4-11eb-0563-673747769a61
+# ╟─9302b00c-656f-11eb-25b3-495ae1c843cc
+# ╟─657c3a98-6573-11eb-1ccb-b1d974414647
+# ╠═8d4cb5dc-6573-11eb-29c8-81baa6e3fffc
+# ╠═d6694c32-656c-11eb-0796-5f485cccccf0
+# ╠═d2813d40-656d-11eb-2cfc-e389ed2a0d84
+# ╟─ce75fe16-6570-11eb-3f3a-577eac7f9ee8
 # ╟─37972f08-db05-4e84-9528-fe16cd86efbf
-# ╠═2d3466df-48b3-432f-843d-8f83d7fb575e
+# ╟─1bd2c660-6572-11eb-268c-732fd2210a58
+# ╟─f4cd5fb2-6574-11eb-37c4-73d4b21c1883
 # ╠═0b35f73f-6976-4d85-b61f-b4188440043e
+# ╟─373cb47e-655e-11eb-2751-0150985d98c1
 # ╟─47ac6d3c-6556-11eb-209d-f7a8219512ee
-# ╟─d910af76-64b2-11eb-14b6-a7f62a6ad82e
-# ╟─f9ce314c-655d-11eb-135b-2df45d413ab1
-# ╠═373cb47e-655e-11eb-2751-0150985d98c1
 # ╠═51a16fcc-6556-11eb-16cc-71a978e02ef0
 # ╠═f6f71c0e-6553-11eb-1a6a-c96f38c7f17b
 # ╠═4a9b5d8a-64b3-11eb-0028-898635af227c
