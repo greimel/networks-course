b1e784949485cf83003c17680d8c768025725179
greimel <fabgrei@gmail.com>
2021-02-23 22:12:48 +0000

    Deploying to gh-pages from  @ 8e234fed1fd419c43923903f5ea386796ae70fa4 🚀

--- notebooks/comparisons.jl
+++ notebooks/comparisons.jl
@@ -75,13 +75,7 @@ end
 
 # ╔═╡ 7fadac3a-6b77-11eb-2030-f92648bcef71
 md"""
-`comparisons.jl` | **Version 0.2** | *last updated: Feb 15*
-"""
-
-# ╔═╡ 1b5e05a6-6d35-11eb-1921-3b5c6836e288
-md"""
-!!! danger "Preliminary version"
-    Nice that you've found this notebook on github. We appreciate your engagement. Feel free to have a look. Please note that the notebook is subject to change.
+`comparisons.jl` | **Version 1.0** | *last updated: Feb 23*
 """
 
 # ╔═╡ 549c17f0-6b77-11eb-3cc7-379d1bcfad6f
@@ -93,24 +87,26 @@ This lecture is loosely based on [this paper](https://www.greimel.eu/static/fall
 #### Lecture Notes
 
 1. We will discuss some evidence that our economic decisions are influenced by our peers. *(Our consumption of visible goods depends on the consumption of our peers' visible goods.)*
+   * [Neighbors of lottery winners buy bigger cars](https://www.aeaweb.org/articles?id=10.1257/aer.101.5.2226)
+   * [Nonrich consume more visible goods when top incomes rise](https://www.mitpressjournals.org/doi/abs/10.1162/REST_a_00613)
+   * [Home owners are less happy with their house when a big house is built nearby](https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3378131)
 
-2. We will model the group of peers as a social network. (Building on the lecture on *Network Games*.)
+2. Discuss how consumption network effects can be estimated [(paper)](https://academic.oup.com/restud/article/87/1/130/5486072)
+
+3. We will model the group of peers as a social network. (Building on the lecture on *Network Games*.)
 
 #### Pluto Notebook
 
-3. We will have a look at the *Distributional National Accounts* data set and visualize rising income inequality and the household debt boom in the US.
-"""
-
-# ╔═╡ 12679864-6f8a-11eb-09e6-fb19d007cb68
-md"""
-4. Use the network model as a potential link between rising income inequality and the household debt boom.
+4. We will have a look at the *Distributional National Accounts* data set and visualize rising income inequality and the household debt boom in the US.
+    * [Data and paper](http://gabriel-zucman.eu/usdina/), [Julia package to manage dataset](https://github.com/greimel/DINA.jl)
+5. Generate a debt boom from rising inequality using our model
 """
 
 # ╔═╡ 93204904-6b7e-11eb-2101-711075dfeb68
 md"""
-# The Distribution of Income and Wealth
+# Distributional National Accounts
 
-We will first take a look at the a few facts from the US distributional national accounts data *(Piketty, Saez & Zucman, 2018)* and then try explain them using our simple model of social comparisons.
+We will first take a look at a few facts from the US distributional national accounts data *(Piketty, Saez & Zucman, 2018)* and then try explain them using our simple model of social comparisons.
 
 """
 
@@ -138,28 +134,35 @@ md"""
 """
 
 # ╔═╡ 076ab958-6b79-11eb-3426-37f1dab6d734
-par = (β = 0.95, ξ = 0.9, ϕ = 0.7, p = 1)
+par = (β = 0.95, ξ = 0.30, ϕ = 0.7, p = 1)
 
 # ╔═╡ 4b3e320e-6b79-11eb-2336-d57c4c0e784c
-h_next, h₀, y₀, groups = let
+h_next, h₀, y₀, groups, m_next = let
 	@unpack β, ξ, ϕ, p = par
 	
 	groups = ["bottom 50", "middle 40", "top 10"]
 	
-	κ₁ = ξ/(1 - ξ) * (1 + β)
+	κ₀ = ξ/(1 - ξ) * (1 + β)
+	κ₁ = κ₀ #ξ/(1 - ξ) * (1 + β)
 	κ₂ = (1 + β)/κ₁ + 1
 	
 	y₀ = [0.5, 1, 2]
 
-	G = [0.5 0.5 0  ;
-		 0   0.5 0.5;
-		 0.  0   0]
+	G = [0  0   0.5;
+		 0  0   0.5;
+		 0.  0  0]
 	
 	h_next(y, h) = (1-β)/(κ₂ * p) * y + ϕ / κ₂ * G * h
 	
+	function m_next(y, h)
+		h_n = h_next(y, h)
+		
+		β .* (y - p/κ₀ * (h_n - ϕ * G * h)) 
+	end
+	
 	h₀ = (I - (ϕ/κ₂ * G)) \	((1-β)/(κ₂ * p) * y₀)
 	
-	(; h_next, h₀, y₀, groups)
+	(; h_next, h₀, y₀, groups, m_next)
 end
 
 # ╔═╡ d2fe0186-6b7b-11eb-02e6-cfb1e163889f
@@ -167,6 +170,7 @@ begin
 	N = length(h₀)
 	T = 10
 	h_sim = zeros(N, T)
+	m_sim = zeros(N, T)
 	h_sim[:, 1] = h₀
 end
 
@@ -174,6 +178,9 @@ end
 begin
 	y_panel = zeros(N, T)
 	y_panel[:, 1] .= y₀
+	
+	m_sim[:, 1] .= m_next(y₀, h₀)
+	
 	for t in 2:T
 		y_panel[1, t] = y_panel[1, t-1]
 		y_panel[2, t] = y_panel[2, t-1]
@@ -181,26 +188,55 @@ begin
 	end
 		
 	for t in 2:T
-		h_sim[:, t] = h_next(y_panel[:, t-1], h_sim[:, t-1])
+		h_sim[:, t] = h_next(y_panel[:, t], h_sim[:, t-1])
+		m_sim[:, t] = m_next(y_panel[:, t], h_sim[:, t-1])
 	end
 end	
 	
 
+# ╔═╡ 1dd8274e-75f5-11eb-060a-d7b5dbb5731a
+y_panel
+
+# ╔═╡ d03b547a-75f4-11eb-2cb1-99c8da1411b7
+begin
+	sim_df = [(; grp=groups[i], t) for i in 1:N, t in 1:T] |> vec |> DataFrame
+	sim_df.y = y_panel |> vec
+	sim_df.h = h_sim |> vec
+	sim_df.m = m_sim |> vec
+	
+	transform!(sim_df, [:h, :y] => ByRow(/) => :house2inc)
+	transform!(sim_df, [:m, :y] => ByRow(/) => :mort2inc)
+	
+	sim_df
+end
+
 # ╔═╡ b860a4d8-6b7c-11eb-27f2-99c8672eb53d
 let
 	fig = Figure()
-	ax_h   = Axis(fig[1,1], title = "Housing")
-	ax_y   = Axis(fig[1,2], title = "Income")
-	ax_h2y = Axis(fig[1,3], title = "Housing/Income")
+	ax_h   = Axis(fig[1,1][2,1], title = "Housing")
+	ax_y   = Axis(fig[1,1][2,3], title = "Income")
+	ax_m   = Axis(fig[1,1][2,2], title = "Mortgage")
 	
-	for i in 1:N
-		lines!(ax_y, y_panel[i,:], label = groups[i], color = wong_colors[i])
-		lines!(ax_h, h_sim[i,:], label = groups[i], color = wong_colors[i])
-		lines!(ax_h2y, h_sim[i,:] ./ y_panel[i,:], label = groups[i], color = wong_colors[i])
+	ax_h2y = Axis(fig[1,1][1,1], title = "%Δ Housing/Income")
+	ax_d2y = Axis(fig[1,1][1,2], title = "%Δ Mortgage/Income")
+	
+	col = wong_colors
+	
+	for (i, gdf) in enumerate(groupby(sim_df, :grp))
+		grp = only(unique(gdf.grp))
+		
+		lines!(ax_y, gdf.t, gdf.y, label = grp, color = col[i])
+		lines!(ax_h, gdf.t, gdf.h, label = grp, color = col[i])
+		lines!(ax_m, gdf.t, gdf.m, label = grp, color = col[i])
+		
+		lines!(ax_h2y, gdf.t, gdf.house2inc ./ gdf.house2inc[1], label = grp, color = col[i])
+		lines!(ax_d2y, gdf.t, gdf.mort2inc ./ gdf.mort2inc[1], label = grp, color = col[i])
 	end
 	
-	leg_attr = (orientation = :horizontal, tellheight = true, tellwidth = false)
-	Legend(fig[0,:], ax_h; leg_attr...)
+	#leg_attr = (orientation = :horizontal, tellheight = true, tellwidth = false)
+	leg_attr = (; tellwidth = false)
+	
+	Legend(fig[1,1][1,3], ax_d2y; leg_attr...)
 	
 	fig
 end
@@ -281,9 +317,6 @@ debt_fig = let d = agg_df
 	
 	fig
 end
-
-# ╔═╡ 7219b104-6f8a-11eb-3ed1-171f91da5c2e
-debt_fig
 
 # ╔═╡ 03b47940-6f73-11eb-32d8-818a673be255
 md"""
@@ -408,7 +441,7 @@ begin df = dina_df
 end
 
 # ╔═╡ 0d061bc8-6f75-11eb-2e73-c354e5f99841
-let
+d2y_fig = let
 	fig = Figure()
 	ax = Axis(fig[1,1], title = "Mortgage-to-income by income group")
 	
@@ -420,6 +453,9 @@ let
 	Legend(fig[1,2], ax)
 	fig
 end
+
+# ╔═╡ 4ccfee7a-75f9-11eb-04c4-cde6a5543f00
+d2y_fig
 
 # ╔═╡ 46cc354c-6f73-11eb-1a28-a5b171989df4
 ineq_fig = let
@@ -438,7 +474,7 @@ ineq_fig = let
 	fig
 end
 
-# ╔═╡ 1982321e-6f8a-11eb-1b0b-8710ceb30b96
+# ╔═╡ 657d5a46-75f9-11eb-2a6a-7579218682da
 ineq_fig
 
 # ╔═╡ 09cabee2-6b78-11eb-29c8-8f616a79d0e5
@@ -451,11 +487,7 @@ TableOfContents()
 
 # ╔═╡ Cell order:
 # ╟─7fadac3a-6b77-11eb-2030-f92648bcef71
-# ╟─1b5e05a6-6d35-11eb-1921-3b5c6836e288
 # ╟─549c17f0-6b77-11eb-3cc7-379d1bcfad6f
-# ╠═1982321e-6f8a-11eb-1b0b-8710ceb30b96
-# ╠═7219b104-6f8a-11eb-3ed1-171f91da5c2e
-# ╟─12679864-6f8a-11eb-09e6-fb19d007cb68
 # ╟─93204904-6b7e-11eb-2101-711075dfeb68
 # ╟─6e871ffa-6f70-11eb-3aa2-ef38243ab266
 # ╠═2a8377c2-6f70-11eb-0685-f179a9cae063
@@ -464,14 +496,18 @@ TableOfContents()
 # ╟─e6cd3534-6f71-11eb-1b98-478ba82441c6
 # ╠═0d061bc8-6f75-11eb-2e73-c354e5f99841
 # ╟─dd06ba2e-6f72-11eb-2bdd-e19bcb59e76e
-# ╠═46cc354c-6f73-11eb-1a28-a5b171989df4
-# ╠═f397be00-6f72-11eb-1600-b74b07053aa6
+# ╟─46cc354c-6f73-11eb-1a28-a5b171989df4
+# ╟─f397be00-6f72-11eb-1600-b74b07053aa6
 # ╟─fcdbad94-6b78-11eb-1f33-4b35fee6b1b9
-# ╠═076ab958-6b79-11eb-3426-37f1dab6d734
 # ╠═4b3e320e-6b79-11eb-2336-d57c4c0e784c
 # ╠═d2fe0186-6b7b-11eb-02e6-cfb1e163889f
 # ╠═f6e061ce-6b7c-11eb-351d-8f890bc5318c
+# ╠═1dd8274e-75f5-11eb-060a-d7b5dbb5731a
+# ╠═d03b547a-75f4-11eb-2cb1-99c8da1411b7
+# ╠═076ab958-6b79-11eb-3426-37f1dab6d734
 # ╠═b860a4d8-6b7c-11eb-27f2-99c8672eb53d
+# ╠═4ccfee7a-75f9-11eb-04c4-cde6a5543f00
+# ╠═657d5a46-75f9-11eb-2a6a-7579218682da
 # ╟─df5509d8-6b77-11eb-1f61-6d9fe9729e46
 # ╠═c4f5a49a-6b78-11eb-3f82-a52bde70c9d2
 # ╟─03b47940-6f73-11eb-32d8-818a673be255
