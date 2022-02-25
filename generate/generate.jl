e8a863d7b45396b238c4bc884c07cb1a1fa5366c
Fabian Greimel <fabgrei@gmail.com>
2022-02-25 16:53:45 +0100

    update website

--- generate/generate.jl
+++ generate/generate.jl
@@ -96,9 +96,9 @@ show_section_number = false
 
 # ╔═╡ 01a2336a-5c04-4d5a-bb0b-a9c704517dbf
 pages = [
-	(page = "/logistics/", title = "Class Logistics"),
 	(page = "/syllabus/", title = "Syllabus"),
-	(page = "/installation/", title = "Software installation")
+	(page = "/installation/", title = "Software installation"),
+	(page = "/cheatsheets/", title = "Cheatsheets"),
 ]
 
 # ╔═╡ c0768146-5ea0-4736-94f8-2c1a2affa922
