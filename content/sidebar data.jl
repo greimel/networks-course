0286f4191c70d0db3e87ca5c74d7fb4ee3ce9db1
Fabian Greimel <fabgrei@gmail.com>
2023-12-18 17:55:11 +0100

    preliminaries => julia-basics

--- content/sidebar data.jl
+++ content/sidebar data.jl
@@ -1,7 +1,7 @@
 Dict(
     :main => [
         "welcome" => collections["welcome"].pages,
-        "Preliminaries" => collections["preliminaries"].pages,
+        "Preliminaries" => collections["julia-basics"].pages,
         #"Module 2: Social Science & Data Science" => collections["module2"].pages,
         #"Module 3: Climate Science" => collections["module3"].pages,
 
